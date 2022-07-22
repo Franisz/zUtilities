6a479b7b1bb212b6638f5bb9ba361a0ec9a3d7cc
Franisz <47374092+Franisz@users.noreply.github.com>
2022-07-22 13:56:41 +0200

    version number

--- zUtilities/Const.h
+++ zUtilities/Const.h
@@ -3,7 +3,7 @@
 
 namespace GOTHIC_ENGINE {
 #define PLUGIN_NAME "ZUTILITIES"
-#define VERSION_NUMBER "0.27"
+#define VERSION_NUMBER "0.28"
 #define printWin(a) ogame->GetTextView()->Printwin(a)
 #define del(x) { delete x; x = nullptr; }
 }
