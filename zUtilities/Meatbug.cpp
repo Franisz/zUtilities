2a95a3e6703d974336db2b14b954961faa95a23c
Franisz <47374092+Franisz@users.noreply.github.com>
2023-02-04 15:13:48 +0100

    update TrampleMeatbug conditions

--- zUtilities/Meatbug.cpp
+++ zUtilities/Meatbug.cpp
@@ -6,29 +6,38 @@ namespace GOTHIC_ENGINE {
   HOOK Hook_oCNpc_OnTouch PATCH( &oCNpc::OnTouch, &oCNpc::OnChrzonszcz );
   void oCNpc::OnChrzonszcz( zCVob* vob ) {
     oCNpc* npc = vob->CastTo<oCNpc>();
+    zCVisual* visual = npc->GetVisual();
 
-    if ( !npc || !this->IsSelfPlayer() || !this->IsHuman() || this->idx == npc->idx || !Options::TrampleMeatbugs ) {
+    if ( !npc || !visual || !this->IsSelfPlayer() || !this->IsHuman() || this->idx == npc->idx || npc->guild != NPC_GIL_MEATBUG || npc->HasFlag( NPC_FLAG_IMMORTAL ) || !Options::TrampleMeatbugs ) {
       THISCALL( Hook_oCNpc_OnTouch )(vob);
       return;
     }
 
-    if ( npc->guild == NPC_GIL_MEATBUG && npc->GetInstanceName().HasWord( "MEATBUG" ) && npc->attribute[NPC_ATR_HITPOINTSMAX] <= 25 ) {
-      // Kill and Exp
-      npc->DoDie( this );
-      if ( npc->attribute[NPC_ATR_HITPOINTS] > 0 )
-        npc->attribute[NPC_ATR_HITPOINTS] = 0;
+    bool largeMdlScale = false;
+    for ( auto i = 0; i < npc->model_scale.Length(); i++ )
+      if ( npc->model_scale[i] > 1.0 )
+        largeMdlScale = true;
 
-      // Sound
-      if ( zCSoundFX* snd = zsound->LoadSoundFX( "CS_IAM_UD_FLX_0" + Z randomizer.Random( 1, 5 ) + ".WAV" ) ) {
-        zCSoundSystem::zTSound3DParams params;
-        zsound->GetSound3DProps( 0, params );
-        zsound->PlaySound3D( snd, vob, 0, &params );
-        snd->Release();
-      }
-
-      //// Blood
-      //oCVisualFX::CreateAndPlay( "BFX_PRESET2", npc, nullptr, 0, 0, 0, 0 );
+    if ( visual->GetVisualName() != "MEATBUG.MDS" || largeMdlScale ) {
+      THISCALL( Hook_oCNpc_OnTouch )(vob);
+      return;
     }
+
+    // Kill and Exp
+    npc->DoDie( this );
+    if ( npc->attribute[NPC_ATR_HITPOINTS] > 0 )
+      npc->attribute[NPC_ATR_HITPOINTS] = 0;
+
+    // Sound
+    if ( zCSoundFX* snd = zsound->LoadSoundFX( "CS_IAM_UD_FLX_0" + Z randomizer.Random( 1, 5 ) + ".WAV" ) ) {
+      zCSoundSystem::zTSound3DParams params;
+      zsound->GetSound3DProps( 0, params );
+      zsound->PlaySound3D( snd, vob, 0, &params );
+      snd->Release();
+    }
+
+    //// Blood
+    //oCVisualFX::CreateAndPlay( "BFX_PRESET2", npc, nullptr, 0, 0, 0, 0 );
 
     THISCALL( Hook_oCNpc_OnTouch )(vob);
   }
