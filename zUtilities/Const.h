59d59f15e425752bef8194dba42f281c8e793e48
met0rii <sebastian.sobek@gmail.com>
2023-12-12 23:54:57 +0100

    version bump

--- zUtilities/Const.h
+++ zUtilities/Const.h
@@ -3,7 +3,7 @@
 
 namespace GOTHIC_ENGINE {
 #define PLUGIN_NAME "ZUTILITIES"
-#define VERSION_NUMBER "0.35"
+#define VERSION_NUMBER "0.36"
 #define printWin(a) ogame->GetTextView()->Printwin(a)
 #define del(x) { delete x; x = nullptr; }
 }
