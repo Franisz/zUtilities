0dad37b20fa7a3a6512867800e3e24a348b149d3
Myszax <myszax14@gmail.com>
2026-02-28 14:27:22 +0100

    Reorder variables and methods in files

--- zUtilities/FocusStatusBar.cpp
+++ zUtilities/FocusStatusBar.cpp
@@ -4,7 +4,7 @@
 namespace GOTHIC_ENGINE {
 	bool FocusStatusBar::IsDistanceWeaponDamageTypeOverwritten = false;
 
-	const oEIndexDamage FocusStatusBar::PROTECTION_DAMAGE_ORDERED_INDEXES[PROTECTION_COUNT] = {
+	const oEIndexDamage FocusStatusBar::ProtectionDamageOrderedIndexes[ProtectionCount] = {
 	oEDamageIndex_Edge,
 	oEDamageIndex_Blunt,
 	oEDamageIndex_Point,
@@ -13,6 +13,45 @@ namespace GOTHIC_ENGINE {
 	oEDamageIndex_Fly,
 	oEDamageIndex_Fall
 	};
+
+	HOOK Ivk_OnDamage_Hit_DistanceWeapon PATCH(&oCNpc::OnDamage_Hit, &oCNpc::OnDamage_Hit_DistanceWeapon);
+	void oCNpc::OnDamage_Hit_DistanceWeapon(oSDamageDescriptor& desc) {
+		THISCALL(Ivk_OnDamage_Hit_DistanceWeapon)(desc);
+
+		if (FocusStatusBar::IsDistanceWeaponDamageTypeOverwritten)
+			return;
+
+		if (desc.pNpcAttacker != player)
+			return;
+
+		if (!desc.pItemWeapon)
+			return;
+
+		if (!DamageMaskHelper::ItemHasDistanceOrMunitionCategoryFlag(desc.pItemWeapon))
+			return;
+
+		if (Options::DistanceWeaponDamageTypeFromIni != desc.enuModeDamage) {
+			DamageMask tmp{};
+			DamageMaskHelper::MarkIntDamageType(desc.enuModeDamage, tmp);
+			Options::DistanceWeaponDamageType = tmp;
+			Options::DistanceWeaponDamageTypeFromIni = desc.enuModeDamage;
+			zoptions->WriteInt(PLUGIN_NAME, "DistanceWeaponDamageType", desc.enuModeDamage, 0);
+		}
+		FocusStatusBar::IsDistanceWeaponDamageTypeOverwritten = true;
+	}
+
+	HOOK Ivk_EquipItem_Union PATCH(&oCNpc::EquipItem, &oCNpc::EquipItem_Union);
+	void oCNpc::EquipItem_Union(oCItem* item) {
+		THISCALL(Ivk_EquipItem_Union)(item);
+
+		if (this != player)
+			return;
+
+		if (!DamageMaskHelper::ItemHasDistanceOrMunitionCategoryFlag(item))
+			return;
+
+		FocusStatusBar::IsDistanceWeaponDamageTypeOverwritten = false;
+	}
 
 	FocusStatusBar::FocusStatusBar() : StatusBar(ogame->focusBar)
 	{
@@ -25,80 +64,6 @@ namespace GOTHIC_ENGINE {
 		iconCache[oEDamageIndex_Point] = "DMGICON_POINT";     // https://game-icons.net/1x1/skoll/bullseye.html
 		iconCache[oEDamageIndex_Fall] = "DMGICON_FALL";       // https://game-icons.net/1x1/sbed/falling.html
 		iconCache[oEDamageIndex_MAX] = "DMGICON_UNKNOWN";     // https://game-icons.net/1x1/lorc/scar-wound.html
-	}
-
-	bool FocusStatusBar::Init() {
-		return true;
-	}
-
-	void FocusStatusBar::Clear() {
-		__super::Clear();
-
-		if (!bar) {
-			return;
-		}
-
-		del(protView);
-	}
-
-	int FocusStatusBar::GetHorizontalProtMargin(int fontY) {
-		return fontY * 0.1f + 30;
-	}
-
-	int FocusStatusBar::GetVerticalProtMargin(int fontY) {
-		return fontY + 85;
-	}
-
-	FocusStatusProtectionPlacement FocusStatusBar::GetProtPlacement() const
-	{
-		if (activeStatusesCount == 1) {
-			return FocusStatusProtectionPlacement::CLOSE;
-		}
-
-		if (Options::TargetProtectionIconPosition == 0) {
-			return FocusStatusProtectionPlacement::TOP;
-		}
-
-		if (activeStatusesCount <= 0) {
-			return FocusStatusProtectionPlacement::NONE;
-		}
-
-		return FocusStatusProtectionPlacement::RIGHT;
-	}
-
-	void FocusStatusBar::PrintValueOutside(zSTRING& str)
-	{
-		int offsetY = bar->vsizey / 2 + valueView->FontY();
-		int x = bar->vposx + bar->vsizex / 2 - valueView->FontSize(str) / 2;
-
-		int y = bar->vposy - offsetY + valueOffsetY;
-
-		valueView->SetFontColor(zCOLOR(valueView->color.r, valueView->color.g, valueView->color.b, bar->alpha));
-		valueView->Print(x, y, str);
-	}
-
-	void FocusStatusBar::MoveFocusBar(int x, int y, oCNpc* npc) {
-		if (!Options::ShowEnemyBarAboveHim)
-			return;
-
-		if (!IsBarActive())
-			return;
-
-		zCCamera* cam = ogame->GetCamera();
-		zVEC3 viewPos = cam->GetTransform(zTCamTrafoType::zCAM_TRAFO_VIEW) * npc->GetPositionWorld();
-		int posx, posy;
-		cam->Project(&viewPos, posx, posy);
-		if (viewPos[VZ] <= cam->nearClipZ)
-			return;
-
-		x = x + screen->FontSize(npc->name[0]) / 2 - bar->vsizex / 2;
-		if (x + bar->vsizex > 8192)
-			x = 8192 - bar->vsizex;
-
-		x = max(0, x);
-		y = max(0, y - screen->FontY() * 1.75);
-
-		bar->SetPos(x, y);
 	}
 
 	bool FocusStatusBar::NeedAdjustPosition(int x, int y, oCNpc* npc) {
@@ -125,6 +90,20 @@ namespace GOTHIC_ENGINE {
 		return Options::ShowEnemyBarAboveHim;
 	}
 
+	bool FocusStatusBar::Init() {
+		return true;
+	}
+
+	void FocusStatusBar::Clear() {
+		__super::Clear();
+
+		if (!bar) {
+			return;
+		}
+
+		del(protView);
+	}
+
 	void FocusStatusBar::Loop() {
 		if (protView) {
 			protView->ClrPrintwin();
@@ -139,69 +118,38 @@ namespace GOTHIC_ENGINE {
 		}
 	}
 
-	const zSTRING& FocusStatusBar::GetIconNameByDamageIndex(const oEIndexDamage& index)
+	bool FocusStatusBar::BuildProtectionModel(oCNpc* npc)
 	{
-		if (index < 0 || index >= oEDamageIndex_MAX)
-			return iconCache[oEDamageIndex_MAX]; // fallback
+		activeStatusesCount = 0;
+		protectionModel.isInFightMode = player->fmode != 0;
+		protectionModel.targetProtectionMode = protectionModel.isInFightMode
+			? (TargetProtectionMode)Options::ShowTargetProtectionInFight
+			: (TargetProtectionMode)Options::ShowTargetProtectionNoFight;
 
-		return iconCache[index];
-	}
-
-	void FocusStatusBar::RenderProtectionIconsClose(const ProtectionLayout& layout)
-	{
-		unsigned char alpha = ogame->hpBar ? ogame->hpBar->alpha : 255;
-		auto data = BuildIconRenderData(activeStatuses[0], alpha);
-
-		IconInfo(layout.startX + layout.margin, layout.startY, layout.iconSize, data.color, data.texture, data.text);
-	}
-
-	void FocusStatusBar::RenderProtectionIconsTop(const ProtectionLayout& layout)
-	{
-		int currentX = layout.startX + bar->vsizex / 2 - layout.totalContentSize / 2;
-		unsigned char alpha = ogame->hpBar ? ogame->hpBar->alpha : 255;
-
-		for (int i = 0; i < activeStatusesCount; ++i)
-		{
-			auto data = BuildIconRenderData(activeStatuses[i], alpha);
-
-			auto icon = IconInfo(currentX, layout.startY, layout.iconSize, data.color, data.texture, data.text);
-			currentX += icon.GetSize() + layout.margin;
-		}
-	}
-
-	void FocusStatusBar::RenderProtectionIconsRight(const ProtectionLayout& layout)
-	{
-		int currentY = layout.startY;
-		unsigned char alpha = ogame->hpBar ? ogame->hpBar->alpha : 255;
-
-		for (int i = 0; i < activeStatusesCount; ++i)
-		{
-			auto data = BuildIconRenderData(activeStatuses[i], alpha);
-
-			IconInfo(layout.startX, currentY, layout.iconSize, data.color, data.texture, data.text);
-			currentY += layout.iconSize + layout.margin;
-		}
-	}
-
-	bool FocusStatusBar::CanRenderProtectionStatus(oCNpc* npc, oEIndexDamage damageIndex) const
-	{
-		if (protectionModel.targetProtectionMode != TargetProtectionMode::All)
-			return true;
-
-		if (Options::HideTargetProtectionZeroValues && npc->GetProtectionByIndex(damageIndex) == 0) {
+		if (protectionModel.targetProtectionMode == TargetProtectionMode::Disabled)
 			return false;
-		}
 
-		switch (damageIndex) {
-		case oEDamageIndex_Fall:
-			return !Options::HideTargetProtectionFallDamage;
-		case oEDamageIndex_Fly:
-			return !Options::HideTargetProtectionFlyDamage;
-		case oEDamageIndex_Fire:
-			return !Options::HideTargetProtectionFireDamage;
-		default:
+		if (npc->HasFlag(NPC_FLAG_IMMORTAL))
+		{
+			protectionModel.renderMode = ProtectionRenderMode::Immortal;
 			return true;
 		}
+
+		FillDamageIndexesBuffer();
+
+		for (int i = 0; i < activeDamageIndexesCount; ++i) {
+			oEIndexDamage index = activeDamageIndexes[i];
+
+			if (CanRenderProtectionStatus(npc, index)) {
+				NpcProtectionStatus& status = activeStatuses[activeStatusesCount++];
+				status.value = npc->GetProtectionByIndex(index);
+				status.damageIndex = index;
+				status.immune = status.value < 0;
+			}
+		}
+		protectionModel.renderMode = ProtectionRenderMode::Normal;
+
+		return activeStatusesCount > 0;
 	}
 
 	void FocusStatusBar::FillDamageIndexesBuffer()
@@ -209,9 +157,9 @@ namespace GOTHIC_ENGINE {
 		activeDamageIndexesCount = 0;
 
 		if (protectionModel.targetProtectionMode == TargetProtectionMode::All) {
-			for (int i = 0; i < PROTECTION_COUNT; ++i)
+			for (int i = 0; i < ProtectionCount; ++i)
 			{
-				activeDamageIndexes[activeDamageIndexesCount++] = PROTECTION_DAMAGE_ORDERED_INDEXES[i];
+				activeDamageIndexes[activeDamageIndexesCount++] = ProtectionDamageOrderedIndexes[i];
 			}
 			return;
 		}
@@ -285,56 +233,6 @@ namespace GOTHIC_ENGINE {
 		}
 	}
 
-	ProtectionIconRenderData FocusStatusBar::BuildIconRenderData(const NpcProtectionStatus& status, unsigned char alpha)
-	{
-		ProtectionIconRenderData data;
-
-		data.text = status.immune ? IMMUNE_ABBREVIATION : zSTRING(status.value);
-
-		data.texture = Options::TargetProtectionIconStyle
-			? crackedShieldTexture
-			: GetIconNameByDamageIndex(status.damageIndex);
-
-		data.color = Colors::GetColorByDamageIndex(status.damageIndex);
-		data.color.alpha = alpha;
-
-		return data;
-	}
-
-	bool FocusStatusBar::BuildProtectionModel(oCNpc* npc)
-	{
-		activeStatusesCount = 0;
-		protectionModel.isInFightMode = player->fmode != 0;
-		protectionModel.targetProtectionMode = protectionModel.isInFightMode
-			? (TargetProtectionMode)Options::ShowTargetProtectionInFight
-			: (TargetProtectionMode)Options::ShowTargetProtectionNoFight;
-
-		if (protectionModel.targetProtectionMode == TargetProtectionMode::Disabled)
-			return false;
-
-		if (npc->HasFlag(NPC_FLAG_IMMORTAL))
-		{
-			protectionModel.renderMode = ProtectionRenderMode::Immortal;
-			return true;
-		}
-
-		FillDamageIndexesBuffer();
-
-		for (int i = 0; i < activeDamageIndexesCount; ++i) {
-			oEIndexDamage index = activeDamageIndexes[i];
-
-			if (CanRenderProtectionStatus(npc, index)) {
-				NpcProtectionStatus& status = activeStatuses[activeStatusesCount++];
-				status.value = npc->GetProtectionByIndex(index);
-				status.damageIndex = index;
-				status.immune = status.value < 0;
-			}
-		}
-		protectionModel.renderMode = ProtectionRenderMode::Normal;
-
-		return activeStatusesCount > 0;
-	}
-
 	void FocusStatusBar::BuildProtectionLayout(ProtectionLayout& layout)
 	{
 		layout.fontY = protView->FontY();
@@ -349,7 +247,7 @@ namespace GOTHIC_ENGINE {
 			return;
 		}
 
-		layout.placement = GetProtPlacement();
+		layout.placement = GetProtectionPlacement();
 		switch (layout.placement)
 		{
 		case FocusStatusProtectionPlacement::TOP:
@@ -357,7 +255,7 @@ namespace GOTHIC_ENGINE {
 			layout.margin = GetHorizontalProtMargin(layout.fontY);
 
 			const int iconSpacing = layout.fontY * 0.1f;
-			const int immuneStringSize = screen->FontSize(IMMUNE_ABBREVIATION);
+			const int immuneStringSize = screen->FontSize(ImmuneAbbreviation);
 
 			for (int i = 0; i < activeStatusesCount; ++i)
 			{
@@ -376,7 +274,7 @@ namespace GOTHIC_ENGINE {
 		{
 			layout.margin = GetVerticalProtMargin(layout.fontY);
 			layout.totalContentSize = activeStatusesCount * (layout.iconSize + layout.margin) - layout.margin;
-			layout.startX = bar->vposx + bar->vsizex + protectionPlacementRightMargin;
+			layout.startX = bar->vposx + bar->vsizex + ProtectionPlacementRightMargin;
 			layout.startY = bar->vposy + bar->vsizey / 2 - layout.iconSize;
 			break;
 		}
@@ -386,7 +284,7 @@ namespace GOTHIC_ENGINE {
 			const int iconSpacing = layout.fontY * 0.1f;
 
 			const int textSize = activeStatuses[0].immune
-				? screen->FontSize(IMMUNE_ABBREVIATION)
+				? screen->FontSize(ImmuneAbbreviation)
 				: screen->FontSize(zSTRING(activeStatuses[0].value));
 
 			layout.totalContentSize = layout.iconSize + iconSpacing + textSize;
@@ -397,6 +295,31 @@ namespace GOTHIC_ENGINE {
 		}
 	}
 
+	FocusStatusProtectionPlacement FocusStatusBar::GetProtectionPlacement() const
+	{
+		if (activeStatusesCount == 1) {
+			return FocusStatusProtectionPlacement::CLOSE;
+		}
+
+		if (Options::TargetProtectionIconPosition == 0) {
+			return FocusStatusProtectionPlacement::TOP;
+		}
+
+		if (activeStatusesCount <= 0) {
+			return FocusStatusProtectionPlacement::NONE;
+		}
+
+		return FocusStatusProtectionPlacement::RIGHT;
+	}
+
+	int FocusStatusBar::GetHorizontalProtMargin(int fontY) const {
+		return fontY * 0.1f + 30;
+	}
+
+	int FocusStatusBar::GetVerticalProtMargin(int fontY) const {
+		return fontY + 85;
+	}
+
 	void FocusStatusBar::RenderProtectionWithLayout(const ProtectionLayout& layout)
 	{
 		if (protectionModel.renderMode == ProtectionRenderMode::Immortal)
@@ -405,7 +328,7 @@ namespace GOTHIC_ENGINE {
 			if (ogame->hpBar)
 				color.alpha = ogame->hpBar->alpha;
 
-			IconInfo(layout.startX + layout.margin, layout.startY, layout.iconSize, color, crackedShieldTexture);
+			IconInfo(layout.startX + layout.margin, layout.startY, layout.iconSize, color, CrackedShieldTexture);
 			return;
 		}
 
@@ -425,42 +348,119 @@ namespace GOTHIC_ENGINE {
 		}
 	}
 
-	HOOK Ivk_OnDamage_Hit_DistanceWeapon PATCH(&oCNpc::OnDamage_Hit, &oCNpc::OnDamage_Hit_DistanceWeapon);
-	void oCNpc::OnDamage_Hit_DistanceWeapon(oSDamageDescriptor& desc) {
-		THISCALL(Ivk_OnDamage_Hit_DistanceWeapon)(desc);
+	bool FocusStatusBar::CanRenderProtectionStatus(oCNpc* npc, oEIndexDamage damageIndex) const
+	{
+		if (protectionModel.targetProtectionMode != TargetProtectionMode::All)
+			return true;
 
-		if (FocusStatusBar::IsDistanceWeaponDamageTypeOverwritten)
-			return;
-
-		if (desc.pNpcAttacker != player)
-			return;
-
-		if (!desc.pItemWeapon)
-			return;
-
-		if (!DamageMaskHelper::ItemHasDistanceOrMunitionCategoryFlag(desc.pItemWeapon))
-			return;
-
-		if (Options::DistanceWeaponDamageTypeFromIni != desc.enuModeDamage) {
-			DamageMask tmp{};
-			DamageMaskHelper::MarkIntDamageType(desc.enuModeDamage, tmp);
-			Options::DistanceWeaponDamageType = tmp;
-			Options::DistanceWeaponDamageTypeFromIni = desc.enuModeDamage;
-			zoptions->WriteInt(PLUGIN_NAME, "DistanceWeaponDamageType", desc.enuModeDamage, 0);
+		if (Options::HideTargetProtectionZeroValues && npc->GetProtectionByIndex(damageIndex) == 0) {
+			return false;
 		}
-		FocusStatusBar::IsDistanceWeaponDamageTypeOverwritten = true;
+
+		switch (damageIndex) {
+		case oEDamageIndex_Fall:
+			return !Options::HideTargetProtectionFallDamage;
+		case oEDamageIndex_Fly:
+			return !Options::HideTargetProtectionFlyDamage;
+		case oEDamageIndex_Fire:
+			return !Options::HideTargetProtectionFireDamage;
+		default:
+			return true;
+		}
 	}
 
-	HOOK Ivk_EquipItem_Union PATCH(&oCNpc::EquipItem, &oCNpc::EquipItem_Union);
-	void oCNpc::EquipItem_Union(oCItem* item) {
-		THISCALL(Ivk_EquipItem_Union)(item);
+	void FocusStatusBar::RenderProtectionIconsClose(const ProtectionLayout& layout)
+	{
+		unsigned char alpha = ogame->hpBar ? ogame->hpBar->alpha : 255;
+		auto data = BuildIconRenderData(activeStatuses[0], alpha);
 
-		if (this != player)
+		IconInfo(layout.startX + layout.margin, layout.startY, layout.iconSize, data.color, data.texture, data.text);
+	}
+
+	void FocusStatusBar::RenderProtectionIconsRight(const ProtectionLayout& layout)
+	{
+		int currentY = layout.startY;
+		unsigned char alpha = ogame->hpBar ? ogame->hpBar->alpha : 255;
+
+		for (int i = 0; i < activeStatusesCount; ++i)
+		{
+			auto data = BuildIconRenderData(activeStatuses[i], alpha);
+
+			IconInfo(layout.startX, currentY, layout.iconSize, data.color, data.texture, data.text);
+			currentY += layout.iconSize + layout.margin;
+		}
+	}
+
+	void FocusStatusBar::RenderProtectionIconsTop(const ProtectionLayout& layout)
+	{
+		int currentX = layout.startX + bar->vsizex / 2 - layout.totalContentSize / 2;
+		unsigned char alpha = ogame->hpBar ? ogame->hpBar->alpha : 255;
+
+		for (int i = 0; i < activeStatusesCount; ++i)
+		{
+			auto data = BuildIconRenderData(activeStatuses[i], alpha);
+
+			auto icon = IconInfo(currentX, layout.startY, layout.iconSize, data.color, data.texture, data.text);
+			currentX += icon.GetSize() + layout.margin;
+		}
+	}
+
+	ProtectionIconRenderData FocusStatusBar::BuildIconRenderData(const NpcProtectionStatus& status, unsigned char alpha)
+	{
+		ProtectionIconRenderData data;
+
+		data.text = status.immune ? ImmuneAbbreviation : zSTRING(status.value);
+
+		data.texture = Options::TargetProtectionIconStyle
+			? CrackedShieldTexture
+			: GetIconNameByDamageIndex(status.damageIndex);
+
+		data.color = Colors::GetColorByDamageIndex(status.damageIndex);
+		data.color.alpha = alpha;
+
+		return data;
+	}
+
+	void FocusStatusBar::MoveFocusBar(int x, int y, oCNpc* npc) {
+		if (!Options::ShowEnemyBarAboveHim)
 			return;
 
-		if (!DamageMaskHelper::ItemHasDistanceOrMunitionCategoryFlag(item))
+		if (!IsBarActive())
 			return;
 
-		FocusStatusBar::IsDistanceWeaponDamageTypeOverwritten = false;
+		zCCamera* cam = ogame->GetCamera();
+		zVEC3 viewPos = cam->GetTransform(zTCamTrafoType::zCAM_TRAFO_VIEW) * npc->GetPositionWorld();
+		int posx, posy;
+		cam->Project(&viewPos, posx, posy);
+		if (viewPos[VZ] <= cam->nearClipZ)
+			return;
+
+		x = x + screen->FontSize(npc->name[0]) / 2 - bar->vsizex / 2;
+		if (x + bar->vsizex > 8192)
+			x = 8192 - bar->vsizex;
+
+		x = max(0, x);
+		y = max(0, y - screen->FontY() * 1.75);
+
+		bar->SetPos(x, y);
+	}
+
+	void FocusStatusBar::PrintValueOutside(zSTRING& str)
+	{
+		int offsetY = bar->vsizey / 2 + valueView->FontY();
+		int x = bar->vposx + bar->vsizex / 2 - valueView->FontSize(str) / 2;
+
+		int y = bar->vposy - offsetY + valueOffsetY;
+
+		valueView->SetFontColor(zCOLOR(valueView->color.r, valueView->color.g, valueView->color.b, bar->alpha));
+		valueView->Print(x, y, str);
+	}
+
+	const zSTRING& FocusStatusBar::GetIconNameByDamageIndex(const oEIndexDamage& index)
+	{
+		if (index < 0 || index >= oEDamageIndex_MAX)
+			return iconCache[oEDamageIndex_MAX]; // fallback
+
+		return iconCache[index];
 	}
 }
--- zUtilities/FocusStatusBar.h
+++ zUtilities/FocusStatusBar.h
@@ -22,12 +22,6 @@ namespace GOTHIC_ENGINE {
 		All = 2
 	};
 
-	struct ProtectionIconRenderData {
-		zSTRING text;
-		zSTRING texture;
-		zCOLOR color;
-	};
-
 	enum class ProtectionRenderMode {
 		Disabled,
 		Immortal,
@@ -50,50 +44,65 @@ namespace GOTHIC_ENGINE {
 		int totalContentSize = 0;
 	};
 
+	struct ProtectionIconRenderData {
+		zSTRING text;
+		zSTRING texture;
+		zCOLOR color;
+	};
+
 	class FocusStatusBar : public StatusBar {
+	public:
+		static bool IsDistanceWeaponDamageTypeOverwritten;
+
+		FocusStatusBar();
+		bool NeedAdjustPosition(int x, int y, oCNpc* npc);
+
+		virtual bool Init() override;
+		virtual void Clear() override;
+		virtual void Loop() override;
+
 	private:
-		static constexpr int PROTECTION_COUNT = 7;
-		static const oEIndexDamage PROTECTION_DAMAGE_ORDERED_INDEXES[PROTECTION_COUNT];
-		zSTRING IMMUNE_ABBREVIATION = "IMM";
-		const zSTRING crackedShieldTexture = zSTRING("ICON_PROTECTIONS"); // https://game-icons.net/1x1/lorc/cracked-shield.html		
-		const int protectionPlacementRightMargin = 250;
+		static constexpr int ProtectionCount = 7;
+		static constexpr int ProtectionPlacementRightMargin = 250;
+
+		static const oEIndexDamage ProtectionDamageOrderedIndexes[ProtectionCount];
+		const zSTRING CrackedShieldTexture = zSTRING("ICON_PROTECTIONS"); // https://game-icons.net/1x1/lorc/cracked-shield.html
+
+		zCView* protView = nullptr;
+
 		oEIndexDamage activeDamageIndexes[oEDamageIndex_MAX]; // not `PROTECTION_COUNT` because of bound safety
 		int activeDamageIndexesCount = 0;
 
 		NpcProtectionStatus activeStatuses[oEDamageIndex_MAX]; // not `PROTECTION_COUNT` because of bound safety
 		int activeStatusesCount = 0;
 
-		zCView* protView;
-		void MoveFocusBar(int x, int y, oCNpc* npc);
-		int GetHorizontalProtMargin(int fontY);
-		int GetVerticalProtMargin(int fontY);
-		FocusStatusProtectionPlacement GetProtPlacement() const;
-		const zSTRING& GetIconNameByDamageIndex(const oEIndexDamage& index);
-		virtual void PrintValueOutside(zSTRING& str) override;
-		void RenderProtectionIconsClose(const ProtectionLayout& layout);
-		void RenderProtectionIconsTop(const ProtectionLayout& layout);
-		void RenderProtectionIconsRight(const ProtectionLayout& layout);
+		ProtectionModel protectionModel = {};
 
 		int valueOffsetY = 0;
+
+		zSTRING ImmuneAbbreviation = "IMM";
 		zSTRING iconCache[oEDamageIndex_MAX + 1];
-		ProtectionModel protectionModel = {};
+
 		bool BuildProtectionModel(oCNpc* npc);
-		void BuildProtectionLayout(ProtectionLayout& layout);
-		void RenderProtectionWithLayout(const ProtectionLayout& layout);
-
-		ProtectionIconRenderData BuildIconRenderData(const NpcProtectionStatus& status, unsigned char alpha);
-
 		void FillDamageIndexesBuffer();
-		bool CanRenderProtectionStatus(oCNpc* npc, oEIndexDamage damageIndex) const;
 		void BuildFightModeDamage(DamageMask& mask);
 		void BuildNoFightModeDamage(DamageMask& mask);
 
-	public:
-		static bool IsDistanceWeaponDamageTypeOverwritten;
-		FocusStatusBar();
-		bool NeedAdjustPosition(int x, int y, oCNpc* npc);
-		virtual void Loop() override;
-		virtual bool Init() override;
-		virtual void Clear() override;
+		void BuildProtectionLayout(ProtectionLayout& layout);
+		FocusStatusProtectionPlacement GetProtectionPlacement() const;
+		int GetHorizontalProtMargin(int fontY) const;
+		int GetVerticalProtMargin(int fontY) const;
+
+		void RenderProtectionWithLayout(const ProtectionLayout& layout);
+		bool CanRenderProtectionStatus(oCNpc* npc, oEIndexDamage damageIndex) const;
+		void RenderProtectionIconsClose(const ProtectionLayout& layout);
+		void RenderProtectionIconsRight(const ProtectionLayout& layout);
+		void RenderProtectionIconsTop(const ProtectionLayout& layout);
+
+		ProtectionIconRenderData BuildIconRenderData(const NpcProtectionStatus& status, unsigned char alpha);
+		void MoveFocusBar(int x, int y, oCNpc* npc);
+		virtual void PrintValueOutside(zSTRING& str) override;
+
+		const zSTRING& GetIconNameByDamageIndex(const oEIndexDamage& index);
 	};
 }
